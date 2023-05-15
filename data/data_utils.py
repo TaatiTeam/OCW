79860cb2dabdea71dda61bceca698816c188514c
Raeid Saqur <raeidsaqur@cs.toronto.edu>
2023-05-15 16:22:52 -0400

    .

---
+++ data/data_utils.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# File: data_utils.py
+# Author: raeidsaqur
+# Email: raeidsaqur@cs.[toronto|princeton].edu
+# Created on: 2023-05-15
+# 
+# This file is part of only_connect_nlp
+# Distributed under terms of the MIT License
